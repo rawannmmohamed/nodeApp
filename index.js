4ae4fc63ef168edcac8aed29936bd5c4229f03e6
rawannmmohamed <rawanmelkhodary@gmail.com>
2026-02-26 00:50:35 +0200

    init commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello from Node.js via Nginx!\n');
+});
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
